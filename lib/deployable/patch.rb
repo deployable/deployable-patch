90186280f4518c5ccdf1d1262310da41b647fc70
deployable <matt@deployable.co>
2014-10-21 05:43:54 +0100

    restore defaults. add meta. move formats to format

--- lib/deployable/patch.rb
+++ lib/deployable/patch.rb
@@ -1,1 +1,6 @@
 require 'deployable/patch/version'
+
+require 'deployable/patch/attr_defaults'
+require 'deployable/patch/ensure_array'
+require 'deployable/patch/instance_class_variables'
+require 'deployable/patch/meta'
